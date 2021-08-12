66ea0a4024fab3166e852d5ff91fe3f1ec79412a
chenguanhong <chenguanhong@come-future.com>
2021-08-12 11:06:39 +0800

    init

---
+++ src/main/java/chenps3/DynamicProxyTest.java
@@ -0,0 +1,37 @@
+package chenps3;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * @Author chenguanhong
+ * @Date 2020/12/15
+ */
+public class DynamicProxyTest {
+
+    interface IA {
+        String getHelloName();
+    }
+
+    public static void main(String[] arges) throws Exception {
+        IA ia = (IA) createObject(IA.class.getName() + "$getHelloName=Abc");
+        System.out.println(ia.getHelloName()); //getHelloName匹配，输出Abc
+        ia = (IA) createObject(IA.class.getName() + "$getTest=Bcd");
+        System.out.println(ia.getHelloName()); //getTest不匹配，输出null
+    }
+
+    //getHelloName只是个示例，不可以写死判断
+
+    //请实现方法createObject
+    public static Object createObject(String str) throws Exception {
+        String className = str.substring(0, str.lastIndexOf("$"));
+        String methodName = str.substring(str.lastIndexOf("$") + 1, str.lastIndexOf("="));
+        String arg = str.substring(str.lastIndexOf("=") + 1);
+        Class<?> cls = Class.forName(className);
+        return Proxy.newProxyInstance(cls.getClassLoader(), new Class[]{cls}, (proxy, method, args) -> {
+            if (methodName.equals(method.getName())) {
+                return arg;
+            }
+            return null;
+        });
+    }
+}
